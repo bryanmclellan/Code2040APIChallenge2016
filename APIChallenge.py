e1b5fa7df61b832b3f7b8a62388e2ac180e651fc
Bryan McLellan <bryanmcl@stanford.edu>
2016-08-26 17:11:10 -0400

    through 4 in python

--- APIChallenge.py
+++ APIChallenge.py
@@ -1,14 +1,44 @@
-import sys
-import requests
+import requests, httplib
 
-print 'Number of arguments:', len(sys.argv), 'arguments.'
-print 'Argument List:', str(sys.argv)
-
-
-if __name__ == "__main__":
-    connectToEndpoint();
+myToken = 'd34c1c19c158b0ddd603f8637c3b32e2'
 
 def connectToEndpoint():
     endpointURL = 'http://challenge.code2040.org/api/register'
-    myToken = 'd34c1c19c158b0ddd603f8637c3b32e2'
-    r = requests.post(endpointURL, data = {token: myToken, })
+    
+    githubURL = 'https://github.com/bryanmclellan/Code2040APIChallenge2016/'
+    r = requests.post(endpointURL, data = {'token': myToken, 'github': githubURL})
+
+def step2():
+    endpointURL = 'http://challenge.code2040.org/api/reverse'
+    r = requests.post(endpointURL, data = {'token': myToken})
+    reversed = r.text[::-1]
+    validateURL = 'http://challenge.code2040.org/api/reverse/validate'
+    r = requests.post(validateURL, data = {'token': myToken, 'string': reversed})
+
+def step3():
+    endpointURL = 'http://challenge.code2040.org/api/haystack'
+    r = requests.post(endpointURL, data = {'token': myToken})
+    dict = r.json()
+    needle = dict['needle']
+    haystack = dict['haystack']
+    index = haystack.index(needle)
+    validateURL = 'http://challenge.code2040.org/api/haystack/validate'
+    r = requests.post(validateURL, data = {'token': myToken, 'needle': index})
+
+def step4():
+    endpointURL = 'http://challenge.code2040.org/api/prefix'
+    req = requests.post(endpointURL, data = {'token': myToken})
+    dict = req.json()
+    prefix = dict['prefix']
+    array = dict['array']
+    myArray =  [a for a in array if a.find(prefix) != 0]
+    validateURL = 'http://challenge.code2040.org/api/prefix/validate'
+    r = requests.post(validateURL, json = {'token': myToken, 'array': myArray})
+
+
+
+
+
+if __name__ == "__main__":
+    step4()
+
